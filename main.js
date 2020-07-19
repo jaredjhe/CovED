6b265a6ec2959886323cdc5ed2860ef94dc94a23
Jared He <66887902+jaredjhe@users.noreply.github.com>
2020-07-18 22:06:26 -0500

    Add subject select form

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let provinceValue, gradeValue, resultingId;
+let subjectValue, provinceValue, gradeValue, resultingId;
+
+let subjectElement = document.getElementById("select-form-subject");
+subjectElement.onchange = function() {
+    subjectValue = subjectElement.value;
+}
 
 let provinceElement = document.getElementById("select-form-province");
 provinceElement.onchange = function() {
@@ -12,14 +17,17 @@ gradeElement.onchange = function() {
 
 let buttonElement = document.getElementById("go-button");
 buttonElement.onclick = function() {
-    if (provinceValue == undefined) {
+    if (subjectValue == undefined) {
+        let subjectLabelElement = document.getElementById("select-form-subject-label");
+        subjectLabelElement.innerHTML = '<span style="color:red;">Please Select a Subject</span>';
+    } else if (provinceValue == undefined) {
         let provinceLabelElement = document.getElementById("select-form-province-label");
         provinceLabelElement.innerHTML = '<span style="color:red;">Please Select a Province</span>';
     } else if (gradeValue == undefined) {
         let provinceLabelElement = document.getElementById("select-form-grade-level-label");
         provinceLabelElement.innerHTML = '<span style="color:red;">Please Select a Grade Level</span>';
     } else {
-        resultingId = "curriculum.html#" + provinceValue + "-" + gradeValue;
+        resultingId = "curriculum.html#" + subjectValue + "-" + provinceValue + "-" + gradeValue;
         buttonElement.href = resultingId;
     }
 }
